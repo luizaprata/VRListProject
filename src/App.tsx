37530bd70290c3b5a0f1dc282027249a2bbe28cd
Luiza Prata <luizaprata@MacBook-Pro-de-Wander.local>
2023-04-27 12:17:03 -0300

    feat: adiciona react query devtools

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
-import {SafeAreaView} from 'react-native';
+import { SafeAreaView } from 'react-native';
 import ContactManagerListScreen from './modules/contact-manager/ContactManagerListScreen';
 
 const queryClient = new QueryClient();
+
+if (__DEV__) {
+  import('react-query-native-devtools').then(({ addPlugin }) => {
+    addPlugin({ queryClient });
+  });
+}
 
 function App(): JSX.Element {
   return (
